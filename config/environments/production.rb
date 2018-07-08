3ff04e89baddc87b10cb6137e71ce1337195e42f
Haritsinh Gohil <555hareet@gmail.com>
2018-07-09 01:47:57 +0530

    change in production.rb file serve_static_files=true set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  #below line added by me(harit) to solve js issue in development
+  config.serve_static_files = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
